e0f0c473862002f9a495f066122fc58072aab545
Mike <mikelarsenis@gmail.com>
2018-11-05 21:37:57 -0700

    Set up SqlLite database.

---
+++ Diot/Diot/Diot/Services/DatabaseService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Diot.Interface;
+using Diot.Models;
+using SQLite;
+using Xamarin.Forms;
+
+namespace Diot.Services
+{
+    public class DatabaseService
+    {
+        #region  Fields
+
+        private static readonly object locker = new object();
+        private readonly SQLiteConnection conn;
+
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="DatabaseService" /> class.
+        /// </summary>
+        public DatabaseService()
+        {
+            conn = DependencyService.Get<IDatabaseController>().GetConnection();
+            lock (locker)
+            {
+                try
+                {
+                    conn.CreateTable<MovieModel>();
+                }
+                catch (Exception e)
+                {
+                    Debug.Write(e.Message);
+                }
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        ///     Gets all movies.
+        /// </summary>
+        public List<MovieModel> GetAllMovies()
+        {
+            lock (locker)
+            {
+                return conn.Table<MovieModel>().ToList();
+            }
+        }
+
+        /// <summary>
+        ///     Saves (updates or inserts) a movie.
+        /// </summary>
+        /// <param name="movie">The movie.</param>
+        public int SaveMovie(MovieModel movie)
+        {
+            lock (locker)
+            {
+                if (movie.Id == 0)
+                {
+                    return conn.Insert(movie);
+                }
+
+                conn.Update(movie);
+                return movie.Id;
+            }
+        }
+
+        /// <summary>
+        ///     Deletes the movie.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        public int DeleteMovie(int id)
+        {
+            lock (locker)
+            {
+                return conn.Delete(id);
+            }
+        }
+
+        /// <summary>
+        ///     Deletes all movies.
+        /// </summary>
+        public List<MovieModel> DeleteAllMovies()
+        {
+            lock (locker)
+            {
+                conn.DropTable<MovieModel>();
+                conn.CreateTable<MovieModel>();
+                return conn.Table<MovieModel>().ToList();
+            }
+        }
+
+        #endregion
+    }
+}
--- Diot/Diot/Diot/ViewModels/LibraryPageViewModel.cs
+++ Diot/Diot/Diot/ViewModels/LibraryPageViewModel.cs
@@ -1,9 +1,38 @@
-﻿using Diot.Interface;
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Diot.Interface;
+using Diot.Models;
+using Prism.Navigation;
+using Xamarin.Forms;
 
 namespace Diot.ViewModels
 {
     public class LibraryPageViewModel : ViewModelBase
     {
+        #region  Fields
+
+        private List<MovieModel> _moviesList = new List<MovieModel>();
+
+        #endregion
+
+        #region Properties
+
+        public ICommand AddNewMovieCommand => new Command(addNewMovie);
+        public ICommand DeleteAllMoviesCommand => new Command(deleteAllMovies);
+
+        private void deleteAllMovies()
+        {
+            MoviesList = DbService.DeleteAllMovies();
+        }
+
+        public List<MovieModel> MoviesList
+        {
+            get => _moviesList;
+            set => SetProperty(ref _moviesList, value);
+        }
+
+        #endregion
+
         #region Methods
 
         #region Constructors
@@ -17,6 +46,20 @@ namespace Diot.ViewModels
         }
 
         #endregion
+
+        private void addNewMovie()
+        {
+            DbService.SaveMovie(new MovieModel {Title = $"Movie {MoviesList.Count + 1}"});
+            MoviesList = DbService.GetAllMovies();
+        }
+
+        public override void OnNavigatingTo(INavigationParameters parameters)
+        {
+            base.OnNavigatingTo(parameters);
+            MoviesList = DbService.GetAllMovies();
+        }
+
+        public string ButtonText => "This is button text";
 
         #endregion
     }
